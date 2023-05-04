c2fec944709f3a10388c33eaafb6afabef1614c6
Paul <pdetrempe@gmail.com>
2023-05-04 09:13:54 -0700

    First cut at type stability warnings in CI pipeline

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using GTOC1
 using Test
 
-@testset "GTOC1.jl" begin
-    # Write your tests here.
+@testset "GTOC1.jl" begin  
+    # define the test set
+    @testset "Type stability tests" begin
+        # test functions for type stability using @typewarn
+        @testset "Functions" for (name, method) in collect(methods(MyModule))
+            @testset "Function $name" begin
+                @typewarn typeof(method(rand(eltype(method.parameters)))) 
+            end
+        end
+        
+        # # test types for type stability
+        # @testset "Types" begin
+        #     @test isconcretetype(MyModule.MyType)
+        #     @test isbits(MyModule.MyType)
+        # end
+    end
+
 end
