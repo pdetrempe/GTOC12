1b5fc7a1f2709ee48be9ace5ba637affd8cbef88
Paul <pdetrempe@gmail.com>
2023-06-19 18:55:10 -0700

    Added current directory to SPICE filepaths

--- src/utils/SPICE_kernels.jl
+++ src/utils/SPICE_kernels.jl
@@ -7,18 +7,18 @@ function download_all_kernels()
 
     ## Only call downloads if you don't already have the SPICE kernels
     # # Download kernels
-    download(LSK, "/deps/naif0012.tls")
-    download(PCK, "/deps/gm_de440.tpc")
-    download(SPK, "/deps/de440.bsp")
+    download(LSK, "./deps/naif0012.tls")
+    download(PCK, "./deps/gm_de440.tpc")
+    download(SPK, "./deps/de440.bsp")
 end
 
 function furnish_all_kernels()
     # Load leap seconds kernel
-    furnsh("deps/naif0012.tls")
+    furnsh("./deps/naif0012.tls")
 
     # Load a planetary ephemeris kernel
-    furnsh("deps/de440.bsp")
+    furnsh("./deps/de440.bsp")
 
     # Load a planetary properties kernel
-    furnsh("deps/gm_de440.tpc")
+    furnsh("./deps/gm_de440.tpc")
 end 
