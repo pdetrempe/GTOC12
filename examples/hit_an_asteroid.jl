3440a00db5e7d1e99f6ee23bfaa3eb5f26f4f9e4
Paul <pdetrempe@gmail.com>
2023-06-24 22:30:27 -0700

    Got shooting method working!

--- examples/hit_an_asteroid.jl
+++ examples/hit_an_asteroid.jl
@@ -32,14 +32,25 @@ function get_asteroid_state(asteroid, ET)
         μ_CB_or_CB_name=μ_☉)
 end
 
+# Helper function to add filler points to start/end span
+function vec_from_span(span; num_points=100)
+    if length(span) == 1
+        vec_out = collect(range( 0, span[1], num_points))
+
+    elseif length(span) == 2
+        vec_out = collect(range( span[1], span[2], num_points))
+        
+    else
+        return span
+    end
+end
+
 # Use that asteroids orbital elements and sweep through fast variable to plot
 # TODO: Move to own plotting file
-function plot_asteroid_from_df_row(asteroid; ETs=[GTOC12.ET₀, GTOC12.ET₀ + 365 * 24 * 3600])
+function plot_asteroid_from_df_row(asteroid; ET_in=[GTOC12.ET₀, GTOC12.ET₀ + 365 * 24 * 3600])
 
     # Accept start/end time or vector of times
-    if length(ETs) == 2
-        ETs = collect(range(ETs[1], ETs[end], 100))
-    end
+    ETs = vec_from_span(ET_in)
 
     # Get asteroid position at all true anomalies
     x_asteroid = hcat([get_asteroid_state(asteroid, ET) for ET in ETs]...)
@@ -67,12 +78,10 @@ end
 
 # Plot Earth trajectory
 planet = "EARTH"
-function plot_planet!(; planet="EARTH", ETs=[GTOC12.ET₀, GTOC12.ET₀ + 365 * 24 * 3600])
+function plot_planet!(; planet="EARTH", ET_in=[GTOC12.ET₀, GTOC12.ET₀ + 365 * 24 * 3600])
 
     # Accept start/end time or vector of times
-    if length(ETs) == 2
-        ETs = collect(range(ETs[1], ETs[end], 100))
-    end
+    ETs = vec_from_span(ET_in)
 
     x_planet = hcat([get_planet_state(planet, ET) for ET in ETs]...)
 
@@ -117,25 +126,53 @@ function calculate_asteroid_miss_distance(x_aug)
     cost = xₜ[1:3] - rₜ_des
 end
 
-# TODO: handle indices more intelligently for packing/unpacking
-x_aug = vcat(x₀, t_shoot, x_target[1:3])
-dr_dx_aug = ForwardDiff.jacobian(calculate_asteroid_miss_distance, x_aug)
-drₜ_dv₀ = dr_dx_aug[:, 4:6] # Variation of final position w.r.t. initial velocity
-drₜ_dTOF = dr_dx_aug[:, 7]  # Variation of final position w.r.t. time-of-flight
-
-# 4. Use Newton to calculate initial DV
-drₜ = calculate_asteroid_miss_distance(x_aug)
-
-dx₀ = -dr_dx_aug[:,4:7]\drₜ
-
-x_aug[4:7] += dx₀
-
-
 # TODO: Think about clever way to package problem into a type
 # such that can have one giant vector for calculating adjoint_sensitivities
 # 
 # 5. Iterate until convergence
-# MAX_ITER = 50
-# num_iter = 0
-# while num_iter < MAX_ITER && drₜ > GTOC12.POS_ABS_TOL
+MAX_ITER = 50
+num_iter = 0
+drₜ = x₀[1:3]
+while num_iter < MAX_ITER && norm(drₜ) > GTOC12.POS_ABS_TOL
+    # Form vector used by ForwardDiff problem
+    x_aug = vcat(x₀, t_shoot, x_target[1:3])
 
+    # Calculate miss distance from current propagation
+    global drₜ = calculate_asteroid_miss_distance(x_aug)
+    println(drₜ)
+
+    # Calculate sensitivity of solution to all parameters
+    dr_dx_aug = ForwardDiff.jacobian(calculate_asteroid_miss_distance, x_aug)
+
+    # Get partials w.r.t. design variables
+    # (In this case, initial velocity and time of flight)
+    ∂rₜ_∂v₀ = dr_dx_aug[:, 4:6] # Variation of final position w.r.t. initial velocity
+    ∂rₜ_∂TOF = dr_dx_aug[:, 7]  # Variation of final position w.r.t. time-of-flight
+
+    # ^^^TODO: handle indices more intelligently for packing/unpacking
+
+
+    # Update initial state via Newton's method
+    # See Pavlak Thesis Eq. 3.12
+    dv₀ = -∂rₜ_∂v₀\drₜ
+    dTOF = -∂rₜ_∂TOF\drₜ
+
+    global x₀[4:6] += dv₀
+
+    # Variable time stuff isn't working interestingly
+    # global t_shoot += dTOF
+
+    global num_iter += 1
+
+end
+
+# Plot spacecraft trajectory
+function plot_coast!(x₀, tspan)
+    tvec = vec_from_span(tspan)
+    states = hcat([propagate_keplerian(x₀, t) for t in tvec]...)
+    
+    plot!(states[1, :], states[2, :], label="coast", color=colormap("Greens"))
+
+end
+
+plot_coast!(x₀, t_shoot)
