da95d9fa5a356ef08016f4ac99d7c700fbafb3d5
Jeff-Robinson <jeffrobinson132@gmail.com>
2023-07-03 11:53:18 -0700

    make TT_start a module-global const

--- src/utils/asteroids.jl
+++ src/utils/asteroids.jl
@@ -31,7 +31,6 @@ function get_asteroid_df()
     asteroid_df.mean_anom = deg2rad.(asteroid_df.mean_anom)
 
     # Add other time epochs that are useful
-    TT_start = "2035, Jan 1, 00:00:00.0000 (TDT)" # TT is TDT in SPICE
     asteroid_df[!, :TT] .= TT_start
     asteroid_df[!, :ET] .= str2et(TT_start)
 
