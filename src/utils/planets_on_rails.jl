4a967006e9f3b52618e4b8973ab9e92e62050591
Jeff-Robinson <jeffrobinson132@gmail.com>
2023-07-06 22:01:30 -0700

    add SOI to planet_df

--- src/utils/planets_on_rails.jl
+++ src/utils/planets_on_rails.jl
@@ -4,15 +4,18 @@ function get_planet_df(plts = [1,2,3,4,5,6,7,8,9,199,299,399])
     planet_df = DataFrame([
         Pair(itr...)
         for itr in zip(
-        [:ID, :NAME, :sma, :ecc, :inc, :LAN, :argperi, :true_anom, :mean_anom],
-        [Int[],String[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[]]
+        [:ID, :NAME, :sma, :ecc, :inc, :LAN, :argperi, :true_anom, :mean_anom, :SOI],
+        [Int[],String[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[]]
     )]
     )
     for idx in eachindex(plts)
-        x_plt=get_planet_state(plts[idx],ET₀)
+        pc = plts[idx]
+        pn = bodc2s(plts[idx])
+        x_plt=get_planet_state(pc,ET₀)
         plt_oe = RV2COE(x⃗=x_plt)
         plt_M = mean_anom(x⃗=x_plt)
-        push!(planet_df,[plts[idx], bodc2s(plts[idx]), plt_oe..., plt_M])
+        plt_SOI = body_SOI(CB=default_CB_str,orbiting_body=pn)
+        push!(planet_df,[pc, pn, plt_oe..., plt_M, plt_SOI])
     end
 
     planet_df[!, :MJD] .= 64328
