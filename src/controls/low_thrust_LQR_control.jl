7b4b83e91c152dea68417992a6ecaa33234e5fe3
Paul <pdetrempe@gmail.com>
2023-07-10 23:50:03 -0700

    Added plot of steered burn.
    Actually looks like it's working

--- src/controls/low_thrust_LQR_control.jl
+++ src/controls/low_thrust_LQR_control.jl
@@ -6,12 +6,12 @@ using ForwardDiff
 # using BoundaryValueDiffEq
 
 # Find asteroid closest (in terms of orbital energy) to the Earth
-_, ID_min = findmin(abs.(GTOC12.asteroid_df.sma / au2m .- 1.))
+_, ID_min = findmin(abs.(GTOC12.asteroid_df.sma / au2m .- 1.0))
 asteroid = Asteroid(ID_min)
 
 # Try out shooting method to hit asteroid
 furnish_all_kernels()
-DV₀ = [-1000.; 0.; 3500.]
+DV₀ = [-1000.0; 0.0; 3500.0]
 x₀ = get_body_state(GTOC12.Earth; ET=GTOC12.ET₀) + [0; 0; 0; DV₀[:]]
 
 # Transfer time
@@ -44,9 +44,9 @@ plot_coast!(x₀⁺, Δt; label="Coast 1", color=colormap("Greens"))
 #states_out, controls_out, time_out = optimize_impulsive_launch(x₀, x_target, Δt; ΔV₀=ΔV_departure_1, asteroid_ID=ID_min)
 
 # Delta V from impulsive 
-  # 1.1632731732968505e-6
-  # 2.408266604131654e-6
-  #-4.037442985551002e-6
+# 1.1632731732968505e-6
+# 2.408266604131654e-6
+#-4.037442985551002e-6
 ## Mining Ship Mass Constraint
 # m0 = md + mp + I*ms <= 3000 kg
 # m0 = 500kg + mp + 20*40 = 1300kg + mp
@@ -58,11 +58,13 @@ plot_coast!(x₀⁺, Δt; label="Coast 1", color=colormap("Greens"))
 function low_thrust_optimal_control!(dstate, state, p, t)
     # unpack parameters
     # no parameters to unpack at this time, could unpack a_grav here (and T i suppose)?
-    _, _, _, μ = p
-    T = 0.6 # Max Thrust 
+    _, _, _, μ, CDU, CTU = p
+    T = 0.6 /(CDU/CTU^2) # Max Thrust (kg-m/s^2)
 
     # unpack state
-    x = state[1:6]; m = state[7]; λ = state[8:13]
+    x = state[1:6]
+    m = state[7]
+    λ = state[8:13]
 
     # Define A and B matrices (time varying)
     # *************************************************
@@ -75,8 +77,8 @@ function low_thrust_optimal_control!(dstate, state, p, t)
     # *************************************************
     #u_star = [0,0,0]
     #δ_star = 0
-    u_star = -B'*λ/norm(B'*λ)  
-    S = norm(B'*λ) .- 1.0        
+    u_star = -B' * λ / norm(B' * λ)
+    S = norm(B' * λ) .- 1.0
     if S > 0
         δ_star = 1.0
     else
@@ -85,20 +87,20 @@ function low_thrust_optimal_control!(dstate, state, p, t)
 
     # Spacecraft Dynamics 
     # *************************************************
-    dx = A + T*δ_star/m * B*u_star
+    dx = A + T * δ_star / m * B * u_star
 
     # Mass variation
     # *************************************************
     # exhaust velocity
-    g0 = 9.81    # m/s^2
-    Isp = 4000.0   # seconds 
+    g0 = 9.81/(CDU/CTU^2)    # m/s^2
+    Isp = 4000.0/CTU   # seconds 
     c = Isp * g0 # specific impulse and grav accel at sea level (m/s)
     #c = 1 
-    dm = -T/c * δ_star
+    dm = -T / c * δ_star
 
     # Costate diff eqs
-    param = m, λ, δ_star, u_star, T, μ 
-    dH_dx = ForwardDiff.gradient(x -> calculate_hamiltonian(x, param ), x) #[1:6])
+    param = m, λ, δ_star, u_star, T, μ
+    dH_dx = ForwardDiff.gradient(x -> calculate_hamiltonian(x, param), x) #[1:6])
     dλ = -dH_dx
 
     # println("dλ $dλ")
@@ -109,7 +111,7 @@ function low_thrust_optimal_control!(dstate, state, p, t)
 
 end
 
-function calculate_hamiltonian(x, p) 
+function calculate_hamiltonian(x, p)
     m, λ, δ_star, u_star, T, μ = p
     # A, B = getAB(x)
     # Solve entire problem in MEE
@@ -124,16 +126,16 @@ function calculate_hamiltonian(x, p)
     # println("size u $(size(u_star))")
     # println("size(λ'*A)  $(size(λ'*A))")
     # println("size(λ'*B*(T/m*δ_star*u_star)) $(λ'*B*(T/m*δ_star*u_star))")
-    H = T/m*δ_star +    λ'*A    +   λ'*B*(T/m*δ_star*u_star)
+    H = T / m * δ_star + λ' * A + λ' * B * (T / m * δ_star * u_star)
     return H
 end
 
 
 
-function bc2!(residual, state, p, t) 
+function bc2!(residual, state, p, t)
     # u[1] is the beginning of the time span, and u[end] is the ending
     # TODO update params to input p_0, p_f
-    x0, m0, xf, μ = p
+    x0, m0, xf, μ, CDU, CTU = p
     # x0, CDU, CTU, μ = get_canonical_state(x0; μ=μ)
     # xf = get_canonical_state(xf, CDU, CTU)
     MEE_init = Cartesian2MEE(x0; μ=μ)
@@ -148,34 +150,54 @@ function bc2!(residual, state, p, t)
     p_f = MEE_target
     # initial boundary value 
     # ***********************************
-    residual[1] = MEE_current_canon[1][1] - p_0[1] 
-    residual[2] = MEE_current_canon[1][2] - p_0[2] 
-    residual[3] = MEE_current_canon[1][3] - p_0[3] 
-    residual[4] = MEE_current_canon[1][4] - p_0[4] 
-    residual[5] = MEE_current_canon[1][5] - p_0[5] 
-    residual[6] = MEE_current_canon[1][6] - p_0[6] 
+    residual[1] = MEE_current_canon[1][1] - p_0[1]
+    residual[2] = MEE_current_canon[1][2] - p_0[2]
+    residual[3] = MEE_current_canon[1][3] - p_0[3]
+    residual[4] = MEE_current_canon[1][4] - p_0[4]
+    residual[5] = MEE_current_canon[1][5] - p_0[5]
+    residual[6] = MEE_current_canon[1][6] - p_0[6]
     residual[7] = state[1][7] - m0
     # final boundary value 
     # ***********************************
-    residual[7]  = MEE_current_canon[end][1] - p_f[1] 
-    residual[8]  = MEE_current_canon[end][2] - p_f[2] 
-    residual[9]  = MEE_current_canon[end][3] - p_f[3] 
-    residual[10] = MEE_current_canon[end][4] - p_f[4] 
-    residual[11] = MEE_current_canon[end][5] - p_f[5] 
-    residual[12] = MEE_current_canon[end][6] - p_f[6] 
+    residual[7] = MEE_current_canon[end][1] - p_f[1]
+    residual[8] = MEE_current_canon[end][2] - p_f[2]
+    residual[9] = MEE_current_canon[end][3] - p_f[3]
+    residual[10] = MEE_current_canon[end][4] - p_f[4]
+    residual[11] = MEE_current_canon[end][5] - p_f[5]
+    residual[12] = MEE_current_canon[end][6] - p_f[6]
 end
 
+
 m = 500.0 # kg
-μ=GTOC12.μ_☉
-MEE_init = Cartesian2MEE(x₀⁺; μ=GTOC12.μ_☉)
+
+# Non-dimensionalize problem
+x0_canon, CDU, CTU, μ_canonical = get_canonical_state(x₀⁺; μ=GTOC12.μ_☉)
+xf_canon = get_canonical_state(x_target, CDU, CTU)
+MEE_init = Cartesian2MEE(x0_canon; μ=μ_canonical)
+
 state_init = vcat(MEE_init, m, ones(6))
-tspan = (0.0,Δt)
-p = (x₀⁺, m, x_target, μ)
+tspan = (0.0, Δt)./CTU
+dt = 24 * 3600.0/CTU
+
+# Pack up parameters and solve
+p = (x0_canon, m, xf_canon, μ_canonical, CDU, CTU)
 bvp2 = TwoPointBVProblem(low_thrust_optimal_control!, bc2!, state_init, tspan, p)
-sol2 = solve(bvp2, Shooting(Tsit5()), dt=24*3600.0, abstol=1e-4, reltol=1e-10) # we need to use the MIRK4 solver for TwoPointBVProblem
+sol = solve(bvp2, Shooting(Vern7()), dt=dt, abstol=1e-6, reltol=1e-10) # we need to use the MIRK4 solver for TwoPointBVProblem
+
+
+
+# Redimensionalize problem
+MEE_out = hcat([state[1:6] for state in sol.u])
+x_spacecraft = MEE2Cartesian.(MEE_out; μ=μ_canonical)
+redimensionalize_state!.(x_spacecraft; CDU=CDU, CTU=CTU)
+r_spacecraft = getindex.(x_spacecraft', 1:3)'
+
+# Plot solution alongside coasts
+plot!(r_spacecraft[:,1], r_spacecraft[:,2], r_spacecraft[:,3], color="cyan", label="steered burn")
+
 # cartesian_solution = MEE2Cartesian.()
 
-MEE_final = Cartesian2MEE(x_target; μ=GTOC12.μ_☉)
+# MEE_final = Cartesian2MEE(x_target; μ=GTOC12.μ_☉)
 
 
 # # Try running continuous burn over this arc
