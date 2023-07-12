d0d9e14b79d3d854d3eb71dad59d7d111d86dd3c
Paul <pdetrempe@gmail.com>
2023-07-11 18:43:16 -0700

    Changes since we both modified asteroid.jl

--- src/utils/constants.jl
+++ src/utils/constants.jl
@@ -1,6 +1,6 @@
 using SPICE
 
-export μ_☉, au2m, ET₀, asteroid_df
+export μ_☉, au2m, ET₀
 
 # From Problem statement appendix
 const μ_☉ = 1.32712440018e11 * (1000)^3 # Sun central body, km³/s² → m³/s²
